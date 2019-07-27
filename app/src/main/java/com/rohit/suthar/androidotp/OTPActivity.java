0bacd4baa9f91059aa75ac09143a3cc2ee92c576
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-27 22:08:53 +0530

    Add OTP Activity

---
+++ app/src/main/java/com/rohit/suthar/androidotp/OTPActivity.java
@@ -0,0 +1,13 @@
+package com.rohit.suthar.androidotp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}

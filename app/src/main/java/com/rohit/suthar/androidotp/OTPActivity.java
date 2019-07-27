44d1b6fa56061290d5198c2de4500b5d304791d1
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-27 22:21:48 +0530

    Add UI in OTP activity

--- app/src/main/java/com/rohit/suthar/androidotp/OTPActivity.java
+++ app/src/main/java/com/rohit/suthar/androidotp/OTPActivity.java
@@ -1,6 +1,6 @@
 package com.rohit.suthar.androidotp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class OTPActivity extends AppCompatActivity {

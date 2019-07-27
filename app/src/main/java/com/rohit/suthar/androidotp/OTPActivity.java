34e1eef612091d3fc0b22b09d142a31c22c8b8e8
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-27 22:31:18 +0530

    Add java code for handling OTP editexts

--- app/src/main/java/com/rohit/suthar/androidotp/OTPActivity.java
+++ app/src/main/java/com/rohit/suthar/androidotp/OTPActivity.java
@@ -1,13 +1,101 @@
 package com.rohit.suthar.androidotp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class OTPActivity extends AppCompatActivity {
-
+    private EditText mEt1, mEt2, mEt3, mEt4, mEt5, mEt6;
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        initialize();
+
+        addTextWatcher(mEt1);
+        addTextWatcher(mEt2);
+        addTextWatcher(mEt3);
+        addTextWatcher(mEt4);
+        addTextWatcher(mEt5);
+        addTextWatcher(mEt6);
+
+    }
+
+    private void initialize() {
+        mEt1 = findViewById(R.id.otp_edit_text1);
+        mEt2 = findViewById(R.id.otp_edit_text2);
+        mEt3 = findViewById(R.id.otp_edit_text3);
+        mEt4 = findViewById(R.id.otp_edit_text4);
+        mEt5 = findViewById(R.id.otp_edit_text5);
+        mEt6 = findViewById(R.id.otp_edit_text6);
+        mContext = OTPActivity.this;
+    }
+
+    private void addTextWatcher(final EditText one) {
+        one.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                switch (one.getId()) {
+                    case R.id.otp_edit_text1:
+                        if (one.length() == 1) {
+                            mEt2.requestFocus();
+                        }
+                        break;
+                    case R.id.otp_edit_text2:
+                        if (one.length() == 1) {
+                            mEt3.requestFocus();
+                        } else if (one.length() == 0) {
+                            mEt1.requestFocus();
+                        }
+                        break;
+                    case R.id.otp_edit_text3:
+                        if (one.length() == 1) {
+                            mEt4.requestFocus();
+                        } else if (one.length() == 0) {
+                            mEt2.requestFocus();
+                        }
+                        break;
+                    case R.id.otp_edit_text4:
+                        if (one.length() == 1) {
+                            mEt5.requestFocus();
+                        } else if (one.length() == 0) {
+                            mEt3.requestFocus();
+                        }
+                        break;
+                    case R.id.otp_edit_text5:
+                        if (one.length() == 1) {
+                            mEt6.requestFocus();
+                        } else if (one.length() == 0) {
+                            mEt4.requestFocus();
+                        }
+                        break;
+                    case R.id.otp_edit_text6:
+                        if (one.length() == 1) {
+                            InputMethodManager inputManager = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+                            inputManager.hideSoftInputFromWindow(OTPActivity.this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                        } else if (one.length() == 0) {
+                            mEt5.requestFocus();
+                        }
+                        break;
+                }
+            }
+        });
     }
 }
